0cdc18c41ff8fabe49a2dd3e7967af24974464b0
高木健吾 <takagiastro@gmail.com>
2022-07-26 21:10:37 +0900

    <Task6-3>Finished task6-3 6-3のvalidationまで完了しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,4 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :article_likes
+  has_many :articles
+  has_many :comments
 end
